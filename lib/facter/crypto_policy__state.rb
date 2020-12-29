bd7e7cbb333bd30e3cf8587ca88e706665a1eaec
Trevor Vaughan <tvaughan@onyxpoint.com>
2020-12-29 09:43:39 -0500

    fact fallback for EL8.0

--- lib/facter/crypto_policy__state.rb
+++ lib/facter/crypto_policy__state.rb
@@ -35,7 +35,15 @@ Facter.add('crypto_policy__state') do
 
       system_state['global_policy_applied'] = !Array(output).grep(%r{is applied}).empty? if output
 
-      system_state['global_policies_available'] = Dir.glob('/usr/share/crypto-policies/policies/*.pol').map { |x| File.basename(x, '.pol') }
+      # This is everything past EL8.0
+      global_policies = Dir.glob('/usr/share/crypto-policies/policies/*.pol').map{ |x| File.basename(x, '.pol') }
+
+      # Fallback for 8.0
+      if global_policies.empty?
+        global_policies = Dir.glob('/usr/share/crypto-policies/*').select{|x| File.directory?(x)}
+      end
+
+      system_state['global_policies_available'] = global_policies
     end
 
     system_state
