1af543b082442d76564c2489b044d388904fe86f
Trevor Vaughan <tvaughan@onyxpoint.com>
2020-12-31 14:15:10 -0500

    (SIMP-8839) Install a minimum package version
    The version of the crypto-policies package that shipped with EL8.0 will
    cause systems to become inaccessible once placed into FIPS mode.
    
    SIMP-8839 #comment update crypto-policies package version

--- manifests/install.pp
+++ manifests/install.pp
@@ -6,11 +6,14 @@
 # @param package_ensure
 #   The 'ensure' parameter for `$packages`
 #
+#   * NOTE: There are issues with `crypto-policies < 20190000` which may render
+#     a FIPS system inaccessible.
+#
 # @author https://github.com/simp/pupmod-simp-crypto_policy/graphs/contributors
 #
 class crypto_policy::install (
   Array[String[1]] $packages       = ['crypto-policies'],
-  String[1]        $package_ensure = simplib::lookup('simp_options::package_ensure', { 'default_value' => 'installed' })
+  String[1]        $package_ensure = simplib::lookup('simp_options::package_ensure', { 'default_value' => '>20190000' })
 ) {
   assert_private()
 
